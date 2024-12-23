e0fe54e4cb2d1d4bba8183871873ea7610867e91
Andrei <homerdias45@gmail.com>
2024-12-23 18:32:25 -0300

    change att

--- .vscode/tabuleiro.js
+++ .vscode/tabuleiro.js
@@ -1,17 +1,20 @@
-// Seleciona os elementos
-const inicio = document.getElementById("inicio")
-const tabuleiro = document.getElementById("tabuleiro")
-const btnJogar = document.getElementById("btn-jogar")
-const btnVoltar = document.getElementById("btn-voltar")
+//elementomostrar e elemento esconder dentro do() funciona igual const document element id
+function trocarTela(elementoMostrar, elementoEsconder) {
+  elementoEsconder.style.display = "none"
+  elementoMostrar.style.display = "block"
+}
 
-// Mostra o tabuleiro ao clicar em "Jogar"
-btnJogar.addEventListener("click", () => {
-  inicio.style.display = "none"
-  tabuleiro.style.display = "block"
+
+document.getElementById("jogar").addEventListener("click", () => {
+  trocarTela(tabuleiro, inicio)
 })
 
-// Volta para a tela inicial ao clicar em "Voltar"
-btnVoltar.addEventListener("click", () => {
-  tabuleiro.style.display = "none"
-  inicio.style.display = "block"
+document.getElementById("voltar").addEventListener("click", () => {
+  trocarTela(inicio, tabuleiro)
+})
+
+document.getElementById("desistir").addEventListener("click", () => {
+  if (confirm("Desistir, você é nob?")) {
+    trocarTela(inicio, tabuleiro)
+  }
 })
